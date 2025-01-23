209669eff900d4d0978bf903bb0ca818abd66660
Vivek Shahi <shahivivek503@gmail.com>
2025-01-23 20:16:07 +0530

    Update function of add to cart and buy button on product page

--- app/product/[productid]/ProductActionButton.tsx
+++ app/product/[productid]/ProductActionButton.tsx
@@ -2,20 +2,44 @@
 
 import { Button } from 'antd'
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { AddProductToCart } from '../../redux/cartSlice';
 
 function ProductActionButton({
     product
 }: {
-    product : any
+    product: any
 }) {
-  return (
-    <div className='flex gap-5 mt-5'>
-        <Button type='default'
-        disabled={product.countInStock <  1}>Add to Cart</Button>
-        <Button type='primary'
-        disabled={product.countInStock < 1}>Buy Now</Button>
-    </div>
-  )
+    const dispatch = useDispatch();
+
+    const handleAddToCart = () => {
+        dispatch(AddProductToCart({ ...product, quantity: 1 }));
+    };
+
+    const handleBuyNow = () => {
+        dispatch(AddProductToCart({ ...product, quantity: 1 }));
+        // Redirect to the checkout page (assuming the route is /checkout)
+        window.location.href = '/cart';
+    };
+
+    return (
+        <div className='flex gap-5 mt-5'>
+            <Button
+                type='default'
+                disabled={product.countInStock < 1}
+                onClick={handleAddToCart}
+            >
+                Add to Cart
+            </Button>
+            <Button
+                type='primary'
+                disabled={product.countInStock < 1}
+                onClick={handleBuyNow}
+            >
+                Buy Now
+            </Button>
+        </div>
+    );
 }
 
-export default ProductActionButton
+export default ProductActionButton;
