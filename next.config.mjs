1775f90bbe395307cec8b1edc33c812fdb78dc8d
Vivek Shahi <shahivivek503@gmail.com>
2026-03-29 00:41:34 +0530

    Update image configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images:{
+//         domains:['localhost', 'firebasestorage.googleapis.com'],
+//     },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['localhost', 'firebasestorage.googleapis.com'],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
