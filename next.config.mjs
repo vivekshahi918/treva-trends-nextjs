9064a9e80fb686d3acdcf8262936b1abc2da745d
Vivek Shahi <shahivivek503@gmail.com>
2026-03-29 01:07:39 +0530

    Remove unused port and pathname from config

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/v0/b/**',
       },
     ],
   },
